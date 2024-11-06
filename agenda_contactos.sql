42cdfcf59186b071510b99b818c772175a3b5258
DavC3ro <luisdavidagudelo04@gmail.com>
2024-11-05 20:29:45 -0500

    nueva base de datos actualizada

--- agenda_contactos.sql
+++ agenda_contactos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-11-2024 a las 05:57:55
+-- Tiempo de generación: 06-11-2024 a las 02:29:18
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
